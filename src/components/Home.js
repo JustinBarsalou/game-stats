2c20173116a09813f75e55ce23b9265ff2b0341d
JustinBarsalou <justinbarsalou@gmail.com>
2020-11-22 14:32:31 -0600

    added api info into app, and have API key hidden

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,25 @@
+import '../assets/css/Home.css'
 function Home() {
-    return <h1>Hello</h1>;
+  const API_KEY = process.env.REACT_APP_API_KEY
+  const options = {
+    method: 'GET',
+    url: 'https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Chob%252321309/battle',
+    headers: {
+      'x-rapidapi-key': API_KEY,
+      'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com'
+    }
+  };
+
+
+    return (
+      <div className='Home'>
+        <section>
+          Hello This is Section
+        </section>
+      </div>
+
+    );
+    
   }
 
 
