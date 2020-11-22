8200db70f1cd186accb253ed70b47cf9bdba63ca
JustinBarsalou <justinbarsalou@gmail.com>
2020-11-22 15:03:28 -0600

    reworking API, using npm module instead of API call, seems theres a module for what i need. saving changed before trying

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from 'react';
 import './App.css';
 import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <h1>Game Stats</h1>
-        </p>
-      </header>
-        <Home></Home>
-    </div>
-  );
+class App extends React.Component {
+
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <h1>Game Stats</h1>
+          </p>
+        </header>
+          <Home></Home>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,40 @@
 import '../assets/css/Home.css'
+// const axios = require('axios');
+
 function Home() {
-  const API_KEY = process.env.REACT_APP_API_KEY
-  const options = {
-    method: 'GET',
-    url: 'https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Chob%252321309/battle',
-    headers: {
-      'x-rapidapi-key': API_KEY,
-      'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com'
-    }
-  };
+
+  // const API_KEY = process.env.REACT_APP_API_KEY
+  // const gamertag = 'kdkt_moist'
+  // const platform = 'xbl'
+
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Chob%252321309/battle',
+  //   headers: {
+  //     'x-rapidapi-key': API_KEY,
+  //     'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com'
+  //   }
+  // };
+
+  // fetch("https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Chob%252321309/battle", {
+  //   "method": "GET",
+  //   "headers": {
+  //     "x-rapidapi-key": API_KEY,
+  //     "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/"+ {gamertag} + "/" + {platform}
+  //   }
+  // })
+  // .then(response => {
+  //   console.log(response);
+  // })
+  // .catch(err => {
+  //   console.error(err);
+  // });
 
 
     return (
       <div className='Home'>
         <section>
-          Hello This is Section
+          Hello i am section 
         </section>
       </div>
 
